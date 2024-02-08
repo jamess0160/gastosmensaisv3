3f5014a6fc9be1a6e798906d583d234023464049
Tiago Ribeiro <tiagoribeiro12@hotmail.com.br>
2024-02-08 11:28:24 -0300

    criação de componente para cabeçalho

---
+++ src/app/inicio/components/header.tsx
@@ -0,0 +1,18 @@
+import { IconButton } from "@mui/material";
+import DataAtual from "./dataAtual";
+import { Settings } from '@mui/icons-material'
+import styles from '../inicio.module.css'
+import Link from "next/link";
+
+export default function Header() {
+    return (
+        <div className={styles.header}>
+            <Link href={"/config"}>
+                <IconButton color="primary" size="large">
+                    <Settings fontSize="large" />
+                </IconButton>
+            </Link>
+            <DataAtual />
+        </div>
+    )
+}
