e20062f6478cab44e4b66a854a32dd0f71e7b929
Tiago Ribeiro <tiagoribeiro12@hotmail.com.br>
2024-02-08 14:40:37 -0300

    mudanças menores no item

--- src/app/inicio/components/ResumeItem/ResumeItem.tsx
+++ src/app/inicio/components/ResumeItem/ResumeItem.tsx
@@ -4,13 +4,13 @@ import Link from "next/link";
 interface ResumeItemProps {
     Name: string
     Value: number
-    href: string
+    href?: string
     color?: string
 }
 
 export default function ResumeItem({ Name, Value, href, color }: ResumeItemProps) {
     return (
-        <Link href={href} style={{ textDecoration: "none", color: "white" }}>
+        <Link href={href || ""} style={{ textDecoration: "none", color: "white" }}>
             <div className={styles.resumeItem} style={color ? { backgroundColor: color } : undefined}>
                 <div>{Name}</div>
                 <div>R$ {Value.toFixed(2)}</div>
