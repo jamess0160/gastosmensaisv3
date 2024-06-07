0675004fc7b8fe8fb372edb198bb1a85ab10326c
Tiago Ribeiro <tiagoribeiro12@hotmail.com.br>
2024-06-06 22:04:32 -0300

    add arquivo de tipagem

---
+++ src/database/UtilTypes.d.ts
@@ -0,0 +1,19 @@
+import { prisma } from "./prisma"
+
+export namespace UtilTypes {
+
+    export interface CreateExpense {
+        EntryDate: string
+        Description: string
+        Type: "Default" | "Fixed" | "Installment"
+        ExpenseDate: string
+        CurrentInstallment: string
+        MaxInstallment: string
+        Price: string
+        IdDestiny: string
+        IdExpenseCategory: string
+        IdBank: string
+    }
+
+    export type PrismaTransaction = Parameters<Parameters<typeof prisma.$transaction>[0]>[0]
+}
