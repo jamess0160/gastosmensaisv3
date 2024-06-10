c15ded1de2205c7ffe043fef289c93f45c58f8ab
Tiago Ribeiro <tiagoribeiro12@hotmail.com.br>
2024-06-10 01:18:59 -0300

    torna prisma publico

--- src/base/baseUseCase.ts
+++ src/base/baseUseCase.ts
@@ -2,7 +2,7 @@ import { prisma } from "@/database/prisma"
 import { UtilTypes } from "../database/UtilTypes"
 
 export abstract class BaseUseCase {
-    protected readonly prisma: UtilTypes.PrismaTransaction
+    public readonly prisma: UtilTypes.PrismaTransaction
 
     constructor(tx?: UtilTypes.PrismaTransaction) {
         this.prisma = tx || prisma
