f223777ba878625ee480ff7c3171531cc33474b7
Tiago Ribeiro <tiagoribeiro12@hotmail.com.br>
2024-06-10 01:20:04 -0300

    refatoração para centralizar no caso de uso

--- src/app/api/expense/route.ts
+++ src/app/api/expense/route.ts
@@ -1,70 +1,17 @@
-import { UtilTypes } from "@/database/UtilTypes";
-import { prisma } from "@/database/prisma";
-import { BaseExpensesUseCases, baseExpensesUseCases } from "@/useCases/BaseExpenses/BaseExpensesUseCases";
-import { DefaultExpensesUseCases } from "@/useCases/DefaultExpenses/DefaultExpensesUseCases";
-import { FixedExpensesUseCases } from "@/useCases/FixedExpenses/FixedExpensesUseCases";
-import { InstallmentExpensesUseCases } from "@/useCases/InstallmentExpenses/InstallmentExpensesUseCases";
-import { baseexpenses } from "@prisma/client";
+import { baseExpensesUseCases } from "@/useCases/BaseExpenses/BaseExpensesUseCases";
 import { NextRequest, NextResponse } from "next/server";
+import { expensesUseCase } from "@/useCases/Expenses/ExpensesUseCase";
 
-
-//#region Create Expense 
-
+// Create expense
 export async function POST(request: NextRequest) {
-    let createExpenseData = await request.json() as UtilTypes.CreateExpense
-
-    return NextResponse.json(await prisma.$transaction(async (tx) => {
-        let baseExpense = await createBaseExpense(tx, createExpenseData)
-
-        if (createExpenseData.Type === "Default") {
-            return NextResponse.json(await createDefaultExpense(tx, baseExpense, createExpenseData))
-        }
-
-        if (createExpenseData.Type === "Fixed") {
-            return NextResponse.json(await createFixedExpense(tx, baseExpense, createExpenseData))
-        }
-
-        if (createExpenseData.Type === "Installment") {
-            return NextResponse.json(await createInstallmentExpense(tx, baseExpense, createExpenseData))
-        }
-    }))
+    return NextResponse.json(await expensesUseCase.CreateExpense.run(await request.json()))
 }
 
-function createBaseExpense(tx: UtilTypes.PrismaTransaction, createExpenseData: UtilTypes.CreateExpense) {
-    return new BaseExpensesUseCases(tx).create({
-        Description: createExpenseData.Description,
-        IdBank: parseInt(createExpenseData.IdBank),
-        IdDestiny: parseInt(createExpenseData.IdDestiny),
-        IdExpenseCategory: parseInt(createExpenseData.IdExpenseCategory),
-        Price: parseFloat(createExpenseData.Price),
-        EntryDate: createExpenseData.EntryDate || undefined,
-    })
+export async function PUT(request: NextRequest) {
+    return NextResponse.json(await expensesUseCase.UpdateExpense.run(await request.json()))
 }
 
-function createDefaultExpense(tx: UtilTypes.PrismaTransaction, baseExpense: baseexpenses, createExpenseData: UtilTypes.CreateExpense) {
-    return new DefaultExpensesUseCases(tx).create({
-        IdBaseExpense: baseExpense.IdBaseExpense,
-        ExpenseDate: createExpenseData.ExpenseDate || undefined,
-    })
-}
-
-function createFixedExpense(tx: UtilTypes.PrismaTransaction, baseExpense: baseexpenses, createExpenseData: UtilTypes.CreateExpense) {
-    return new FixedExpensesUseCases(tx).create({
-        IdBaseExpense: baseExpense.IdBaseExpense,
-        StartDate: createExpenseData.ExpenseDate || undefined,
-    })
-}
-
-function createInstallmentExpense(tx: UtilTypes.PrismaTransaction, baseExpense: baseexpenses, createExpenseData: UtilTypes.CreateExpense) {
-    return new InstallmentExpensesUseCases(tx).create({
-        IdBaseExpense: baseExpense.IdBaseExpense,
-        CurrentInstallment: parseInt(createExpenseData.CurrentInstallment),
-        MaxInstallment: parseInt(createExpenseData.MaxInstallment),
-    })
-}
-
-//#endregion
-
+// Delete expense
 export async function DELETE(request: NextRequest) {
     let { searchParams } = new URL(request.url)
     let IdBaseExpense = searchParams.get('IdBaseExpense')
