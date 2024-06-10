36c5c75d131db2f13344c1080d36d965ad1fbf91
Tiago Ribeiro <tiagoribeiro12@hotmail.com.br>
2024-06-09 22:27:00 -0300

    refatoração

--- src/database/prisma.ts
+++ src/database/prisma.ts
@@ -1,11 +1,27 @@
+import { socketUseCases } from '@/useCases/Socket/SocketUseCases'
 import { PrismaClient } from '@prisma/client'
-
-const prismaClientSingleton = () => {
-    return new PrismaClient()
-}
 
 declare global {
     var singleTon_Prisma: undefined | ReturnType<typeof prismaClientSingleton>
+}
+
+function prismaClientSingleton() {
+    return new PrismaClient().$extends({
+        query: {
+            $allModels: {
+                $allOperations: async (data) => {
+                    let result = await data.query(data.args)
+
+                    if (data.operation.includes("find") === false) {
+                        socketUseCases.emmitReloadRequest("/inicio")
+                        socketUseCases.emmitReloadRequest("/categorias")
+                    }
+
+                    return result
+                }
+            }
+        }
+    })
 }
 
 export const prisma = globalThis.singleTon_Prisma ?? prismaClientSingleton()
