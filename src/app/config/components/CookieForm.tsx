6d8d27d0c023704ed513fbd5d4f2e0b4881896f5
Tiago Ribeiro <tiagoribeiro12@hotmail.com.br>
2024-06-12 14:49:46 -0300

    muda componentes para padrão da aplicação

--- src/app/config/components/CookieForm.tsx
+++ src/app/config/components/CookieForm.tsx
@@ -1,40 +1,44 @@
 'use client'
 
-import { UseFormSetValue, useForm } from "react-hook-form-mui"
+import { useForm } from "react-hook-form-mui"
 import { configEvents } from "../events/events"
-import { Dispatch, useState } from "react"
+import { useState } from "react"
 import { UtilTypes } from "@/database/UtilTypes"
-import { Button, CircularProgress, Input, InputLabel, MenuItem, Select, SelectChangeEvent } from "@mui/material"
+import { Button, CircularProgress } from "@mui/material"
 import { clientUtilsUseCases } from "@/useCases/Utils/ClientUtilsUseCases"
+import styles from '../Config.module.css'
 
 export default function CookieForm(props: CookieFormProps) {
     let [isLoading, setLoading] = useState(false)
-    let [selectValue, setSelectValue] = useState(props.month)
 
-    let { register, handleSubmit, setValue } = useForm<UtilTypes.CookiesPostBody>({ defaultValues: { month: props.month, year: props.year } })
+    let { register, handleSubmit } = useForm<UtilTypes.CookiesPostBody>({ defaultValues: { month: props.month, year: props.year } })
 
-    let monthsSelectItems = clientUtilsUseCases.months.map((item, index) => <MenuItem key={index} value={index.toString()}>{item}</MenuItem>)
+    let monthsSelectItems = clientUtilsUseCases.months.map((item, index) => <option key={index} value={index.toString()}>{item}</option>)
 
     if (isLoading) {
         return <CircularProgress />
     }
 
     return (
-        <form className="rounded-lg bg-slate-800 bg-opacity-50 w-1/2 max-md:w-11/12 flex flex-col gap-5 p-5" onSubmit={handleSubmit((data) => configEvents.onCookieChange(data, setLoading))}>
-            <InputLabel className="text-white">Mês</InputLabel >
-            <Select className="text-white" value={selectValue} onChange={onSelectChange.bind(null, setSelectValue, setValue)} >{monthsSelectItems}</Select>
+        <div className="rounded-lg w-1/3 max-md:w-11/12 flex justify-center" style={{ backgroundColor: "#1E1F22" }}>
+            <form className="w-10/12 flex flex-col gap-5 p-5 pt-10" onSubmit={handleSubmit((data) => configEvents.onCookieChange(data, setLoading))}>
 
-            <InputLabel className="text-white">Ano</InputLabel >
-            <Input className="text-white" type="number" {...register("year")} />
+                <div className={styles.campo}>
+                    <legend>Mês</legend>
+                    <select {...register("month")}>
+                        {monthsSelectItems}
+                    </select>
+                </div>
 
-            <Button type="submit" variant="contained" className="w-1/3 m-auto">Enviar</Button>
-        </form>
+                <div className={styles.campo}>
+                    <legend>Ano</legend>
+                    <input {...register("year")}></input>
+                </div>
+
+                <Button type="submit" variant="contained" className="w-1/2 m-auto">Enviar</Button>
+            </form>
+        </div>
     )
-}
-
-function onSelectChange(setSelectValue: Dispatch<string>, setValue: UseFormSetValue<UtilTypes.CookiesPostBody>, data: SelectChangeEvent<string>) {
-    setSelectValue(data.target.value)
-    setValue("month", data.target.value)
 }
 
 interface CookieFormProps {
