5369fcb69949a5f135cb4cdc27cd000b51901866
Tiago Ribeiro <tiagoribeiro12@hotmail.com.br>
2024-06-12 21:11:16 -0300

    ajustes de css do tile

--- src/app/inicio/components/ResumeItem/ResumeItem.tsx
+++ src/app/inicio/components/ResumeItem/ResumeItem.tsx
@@ -10,19 +10,21 @@ interface ResumeItemProps {
 
 export default function ResumeItem({ Name, Value, href, color = "#232323" }: ResumeItemProps) {
 
+    let defaultClass = styles.resumeItem + " pt-2 pb-6 px-6"
+
     if (href) {
         return (
             <Link href={href} className='max-md:w-full' style={{ textDecoration: "none", color: "white" }}>
-                <div className={styles.resumeItem + " max-md:!w-full"} style={color ? { backgroundColor: color } : {}}>
-                    <div className='w-fit m-auto pt-4 font-light'>{Name}</div>
-                    <div className='w-fit m-auto pt-5 font-bold'>R$ {Value.toFixed(2)}</div>
+                <div className={defaultClass + "  max-md:!w-full max-md:!h-auto"} style={{ backgroundColor: color }}>
+                    <div className='w-fit m-auto font-light'>{Name}</div>
+                    <div className='w-fit m-auto mt-3 md:pt-3 font-bold'>R$ {Value.toFixed(2)}</div>
                 </div>
             </Link>
         )
     }
 
     return (
-        <div className={styles.resumeItem + " pt-2 pb-6 px-6"} style={{ backgroundColor: color, height: "auto" }}>
+        <div className={defaultClass} style={{ backgroundColor: color, height: "auto" }}>
             <div className='w-fit font-light text-gray-300'>{Name}</div>
             <div className='w-fit m-auto mt-3 font-bold'>R$ {Value.toFixed(2)}</div>
         </div>
