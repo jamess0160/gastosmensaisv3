3fb772e9af642ae48061b2742414b98a0b7dd486
Tiago Ribeiro <tiagoribeiro12@hotmail.com.br>
2024-06-13 21:07:52 -0300

    retira botão do cookie form

--- src/app/config/components/CookieForm.tsx
+++ src/app/config/components/CookieForm.tsx
@@ -2,9 +2,9 @@
 
 import { useForm } from "react-hook-form-mui"
 import { configEvents } from "../events/events"
-import { useState } from "react"
+import { useRef, useState } from "react"
 import { UtilTypes } from "@/database/UtilTypes"
-import { Button, CircularProgress } from "@mui/material"
+import { CircularProgress } from "@mui/material"
 import { clientUtilsUseCases } from "@/useCases/Utils/ClientUtilsUseCases"
 import styles from '../Config.module.css'
 
@@ -13,6 +13,10 @@ export default function CookieForm(props: CookieFormProps) {
 
     let { register, handleSubmit } = useForm<UtilTypes.CookiesPostBody>({ defaultValues: { month: props.month, year: props.year } })
 
+    let formRef = useRef<HTMLFormElement>(null)
+
+    let submitForm = handleSubmit((data) => configEvents.onCookieChange(data, setLoading))
+
     let monthsSelectItems = clientUtilsUseCases.months.map((item, index) => <option key={index} value={index.toString()}>{item}</option>)
 
     if (isLoading) {
@@ -20,8 +24,13 @@ export default function CookieForm(props: CookieFormProps) {
     }
 
     return (
-        <div className="rounded-lg w-1/3 max-md:w-11/12 flex justify-center" style={{ backgroundColor: "#1E1F22" }}>
-            <form className="w-10/12 flex flex-col gap-5 p-5 pt-10" onSubmit={handleSubmit((data) => configEvents.onCookieChange(data, setLoading))}>
+        <div className="rounded-lg w-1/3 max-md:w-11/12 flex justify-center" style={{ backgroundColor: "#1E1F22" }} >
+            <form
+                className="w-10/12 flex flex-col gap-5 p-5 pt-10"
+                onSubmit={submitForm}
+                ref={formRef}
+                onChange={submitForm}
+            >
 
                 <div className={styles.campo}>
                     <legend>Mês</legend>
@@ -35,7 +44,6 @@ export default function CookieForm(props: CookieFormProps) {
                     <input {...register("year")}></input>
                 </div>
 
-                <Button type="submit" variant="contained" className="w-1/2 m-auto">Enviar</Button>
             </form>
         </div>
     )
