db1c7668da7828a4f529bf98e0bccf8e4c24cff3
Tiago Ribeiro <tiagoribeiro12@hotmail.com.br>
2024-10-18 17:42:31 -0300

    add IdUser

--- src/app/api/cashInflow/clone/route.ts
+++ src/app/api/cashInflow/clone/route.ts
@@ -1,12 +1,13 @@
 import { cashInflowsUseCases } from "@/useCases/CashInflows/CashInflowsUseCases";
 import { serverUtilsUseCases } from "@/useCases/Utils/ServerUtilsUseCases";
 import { revalidatePath } from "next/cache";
-import { NextResponse } from "next/server";
+import { NextRequest, NextResponse } from "next/server";
 
-export async function POST() {
+export async function POST(request: NextRequest) {
     let { month, year } = serverUtilsUseCases.getMonthYear()
+    let IdUser = request.headers.get("IdUser")
 
-    await cashInflowsUseCases.clone(month - 1, year)
+    await cashInflowsUseCases.clone(month - 1, year, Number(IdUser))
 
     revalidatePath("/config")
 
