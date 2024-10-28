a940a1f4b13666316b4e279a3f81a8fb911ca283
Tiago Ribeiro <tiagoribeiro12@hotmail.com.br>
2024-10-28 10:22:55 -0300

    ajuste de tipagem

--- src/app/pages/components/ExpenseForm/ExpenseForm.tsx
+++ src/app/pages/components/ExpenseForm/ExpenseForm.tsx
@@ -2,16 +2,16 @@ import { Dispatch, useState } from "react"
 import { Button, CircularProgress, Dialog, DialogTitle } from "@mui/material"
 import styles from './ExpenseForm.module.css'
 import { useForm } from "react-hook-form"
-import { UtilTypes } from "@/database/UtilTypes"
 import { expenseFormEventsEvents } from "./events/events"
 import FormFields from "./components/FormFields"
 import FormButtons from "./components/FormButtons"
 import { banks, destinys, expensecategories } from "@prisma/client";
+import { CreateTypes } from "@/database/CreateTypes"
 
 //#region Functions 
 
 export default function ExpenseForm(props: FormProps) {
-    let { register, handleSubmit, setValue, reset, formState } = useForm<UtilTypes.CreateExpense>({ defaultValues: props.editItem })
+    let { register, handleSubmit, setValue, reset, formState } = useForm<CreateTypes.CreateExpense>({ defaultValues: props.editItem })
     let [isLoading, setIsLoading] = useState(false)
 
     if (isLoading) {
@@ -25,7 +25,11 @@ export default function ExpenseForm(props: FormProps) {
                 onSubmit={handleSubmit((data) => expenseFormEventsEvents.onSubmit(data, props.setFormState, reset, setIsLoading, Boolean(props.editItem)))}
                 className={styles.form}
             >
-                {Object.values(formState.errors).length > 0 && <div className="mb-5 text-red-600 text-center">Por favor, preencha todos os campos para continuar!</div>}
+                {Object.values(formState.errors).length > 0 && (
+                    <div className="mb-5 text-red-600 text-center">
+                        Por favor, preencha todos os campos para continuar!
+                    </div>
+                )}
                 <FormFields register={register} setValue={setValue} fieldsData={props.fieldsData} editItem={props.editItem} />
                 <FormButtons setFormState={props.setFormState} edit={Boolean(props.editItem)} />
             </form>
@@ -84,7 +88,7 @@ export interface FormProps {
     formState: boolean
     setFormState: Dispatch<boolean>
     fieldsData: FieldsData
-    editItem?: Partial<UtilTypes.CreateExpense>
+    editItem?: Partial<CreateTypes.CreateExpense>
 }
 
 export interface FieldsData {
