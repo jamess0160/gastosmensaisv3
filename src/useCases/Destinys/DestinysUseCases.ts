0a7dc0dff70498568eb906d84d47538bcc7037fa
Tiago Ribeiro <tiagoribeiro12@hotmail.com.br>
2024-10-28 10:45:56 -0300

    add funções para CUD

--- src/useCases/Destinys/DestinysUseCases.ts
+++ src/useCases/Destinys/DestinysUseCases.ts
@@ -15,8 +15,27 @@ export class DestinysUseCases extends BaseUseCase {
     getBy(where: WhereArgs) {
         return this.prisma.destinys.findMany({ where })
     }
+
+    create(data: CreateData) {
+        return this.prisma.destinys.create({ data })
+    }
+
+    update(IdDestiny: number, data: CreateData) {
+        return this.prisma.destinys.update({
+            where: { IdDestiny },
+            data: data
+        })
+    }
+
+    remove(IdDestiny: number) {
+        return this.prisma.destinys.delete({
+            where: { IdDestiny }
+        })
+    }
 }
 
 export const destinysUseCases = new DestinysUseCases()
 
 type WhereArgs = NonNullable<Parameters<typeof prisma.destinys.findFirst>['0']>['where']
+
+type CreateData = Parameters<typeof prisma.destinys.create>['0']['data']
--- src/useCases/ExpenseCategories/ExpenseCategoriesUseCases.ts
+++ src/useCases/ExpenseCategories/ExpenseCategoriesUseCases.ts
@@ -1,4 +1,5 @@
 import { BaseUseCase } from "@/base/baseUseCase";
+import { prisma } from "@/database/prisma";
 
 export class ExpenseCategoriesUseCases extends BaseUseCase {
     getAllByUser(IdUser: number) {
@@ -6,6 +7,25 @@ export class ExpenseCategoriesUseCases extends BaseUseCase {
             where: { IdUser }
         })
     }
+
+    create(data: CreateData) {
+        return this.prisma.expensecategories.create({ data })
+    }
+
+    update(IdExpenseCategory: number, data: CreateData) {
+        return this.prisma.expensecategories.update({
+            where: { IdExpenseCategory },
+            data: data
+        })
+    }
+
+    remove(IdExpenseCategory: number) {
+        return this.prisma.expensecategories.delete({
+            where: { IdExpenseCategory }
+        })
+    }
 }
 
 export const expenseCategoriesUseCases = new ExpenseCategoriesUseCases()
+
+type CreateData = Parameters<typeof prisma.expensecategories.create>['0']['data']
