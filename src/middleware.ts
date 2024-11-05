b0aacb5b0a1923d1e853d70ceda6cb475d853869
Tiago Ribeiro <tiagoribeiro12@hotmail.com.br>
2024-11-05 19:11:58 -0300

    .

--- src/middleware.ts
+++ src/middleware.ts
@@ -14,9 +14,6 @@ export async function middleware(request: NextRequest) {
         return NextResponse.next()
     }
 
-    console.log(request.nextUrl.pathname)
-
-
     let auth = request.headers.get("authorization");
 
     if (!auth) {
