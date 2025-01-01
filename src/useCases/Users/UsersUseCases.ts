3f176cce49803f33a52bd67d27aa6e19e0f9e4b0
Tiago Ribeiro <tiagoribeiro12@hotmail.com.br>
2025-01-01 14:42:06 -0300

    add get e update de users

--- src/useCases/Users/UsersUseCases.ts
+++ src/useCases/Users/UsersUseCases.ts
@@ -1,6 +1,14 @@
 import { BaseUseCase } from "@/base/baseUseCase";
+import { prisma } from "@/database/prisma";
 
 export class UsersUseCases extends BaseUseCase {
+
+    getById(IdUser: number) {
+        return this.prisma.users.findFirst({
+            where: { IdUser }
+        })
+    }
+
     login(login: string, password: string) {
         return this.prisma.users.findFirst({
             where: {
@@ -9,6 +17,19 @@ export class UsersUseCases extends BaseUseCase {
             }
         })
     }
+
+    update(IdUser: number, data: UpdateUsers) {
+        return this.prisma.users.update({
+            where: { IdUser },
+            data: data
+        })
+    }
 }
 
 export const usersUseCases = new UsersUseCases()
+
+//#region Interfaces / Types 
+
+type UpdateUsers = Parameters<typeof prisma.users.update>[0]['data']
+
+//#endregion
