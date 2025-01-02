700fa0ceaf17e6ef4bab9f0b5b215dad06fef4bd
Tiago Ribeiro <tiagoribeiro12@hotmail.com.br>
2025-01-02 11:17:02 -0300

    add auto login

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,13 @@
+import { serverUtilsUseCases } from "@/useCases/Utils/ServerUtilsUseCases/ServerUtilsUseCases";
 import { redirect } from "next/navigation";
 
-export default function Page() {
-    redirect("/pages/login")
+export default async function Page() {
+
+    let session = await serverUtilsUseCases.Cookies.getSession()
+
+    if (session?.IdUser) {
+        redirect("/pages/inicio")
+    } else {
+        redirect("/pages/login")
+    }
 }
