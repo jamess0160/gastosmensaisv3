f4f558ecee2e5da9c1ef2f78b327057a38205f13
Tiago Ribeiro <tiagoribeiro12@hotmail.com.br>
2025-01-01 21:54:14 -0300

    add funções necessárias

--- src/useCases/UsersAuth/UsersAuthUseCases.ts
+++ src/useCases/UsersAuth/UsersAuthUseCases.ts
@@ -7,6 +7,20 @@ export class UsersAuthUseCases extends BaseUseCase {
         return this.prisma.usersauth.findMany()
     }
 
+    async getByToken(Token: string) {
+        return this.prisma.usersauth.findFirst({
+            where: { Token },
+            orderBy: { IdUserAuth: "desc" }
+        })
+    }
+
+    async getByUser(IdUser: number) {
+        return this.prisma.usersauth.findMany({
+            where: { IdUser },
+            orderBy: { IdUserAuth: "desc" }
+        })
+    }
+
     async create(data: CreateUsersAuth) {
         let currentAuth = await this.prisma.usersauth.findMany({
             where: { Token: data.Token }
@@ -18,6 +32,13 @@ export class UsersAuthUseCases extends BaseUseCase {
 
         return this.prisma.usersauth.create({ data })
     }
+
+    update(IdUserAuth: number, data: UpdateUsersAuth) {
+        return this.prisma.usersauth.update({
+            where: { IdUserAuth },
+            data: data
+        })
+    }
 }
 
 export const usersAuthUseCases = new UsersAuthUseCases()
@@ -25,5 +46,7 @@ export const usersAuthUseCases = new UsersAuthUseCases()
 //#region Interfaces / Types 
 
 type CreateUsersAuth = Parameters<typeof prisma.usersauth.create>[0]['data']
+
+type UpdateUsersAuth = Parameters<typeof prisma.usersauth.update>[0]['data']
 
 //#endregion
