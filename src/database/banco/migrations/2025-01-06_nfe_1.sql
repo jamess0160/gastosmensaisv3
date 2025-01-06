9fa3901637e8aefe15f7a3d068639d96835037da
Tiago Ribeiro <tiagoribeiro12@hotmail.com.br>
2025-01-06 11:47:36 -0300

    ajustes banco

---
+++ src/database/banco/migrations/2025-01-06_nfe_1.sql
@@ -0,0 +1,19 @@
+CREATE TABLE `nfeexpenses` (
+    `IdNfeExpense` INT PRIMARY KEY AUTO_INCREMENT,
+    `ExpenseDate` DATETIME NOT NULL DEFAULT (CURRENT_DATE),
+    `DanfeCode` varchar(255),
+    `Company` varchar(255),
+    `IdBaseExpense` INT NOT NULL UNIQUE,
+    FOREIGN KEY (`IdBaseExpense`) REFERENCES `BaseExpenses`(`IdBaseExpense`) ON DELETE CASCADE
+);
+
+CREATE TABLE `nfeitems` (
+    `IdNfeItem` INT PRIMARY KEY AUTO_INCREMENT,
+    `IdNfeExpense` INT NOT NULL,
+    `Description` varchar(255),
+    `Quantity` float,
+    `UN` varchar(255),
+    `UnityValue` float,
+    `TotalValue` float,
+    FOREIGN KEY (`IdNfeExpense`) REFERENCES `nfeexpenses`(`IdNfeExpense`) ON DELETE CASCADE
+);
