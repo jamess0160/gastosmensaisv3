d65897691a0e398e72173360f6bab3fa5f0bc18e
Tiago Ribeiro <tiagoribeiro12@hotmail.com.br>
2025-01-06 15:33:31 -0300

    add nfe popup para listar items de notas fiscais

---
+++ src/app/pages/components/ExpenseTable/CategoryTableRow/components/NfePopUp.tsx
@@ -0,0 +1,74 @@
+import { NfeExpenseChild } from "@/useCases/BaseExpenses/generateFullBaseExpenseChild"
+import { IconButton, Dialog, Table, TableBody, TableCell, TableContainer, TableRow } from "@mui/material"
+import { defaultCellClass } from "../CategoryTableRow"
+import { Close } from "@mui/icons-material"
+import { clientUtilsUseCases } from "@/useCases/Utils/ClientUtilsUseCases/ClientUtilsUseCases"
+import { Dispatch, SetStateAction } from "react"
+
+export function NfePopUp(props: ComponentsProps) {
+
+    const tableRows = generateTableRows(props.item)
+
+    const closeClick = () => {
+        props.setOpen(false)
+    }
+
+    return (
+        <Dialog open={props.open} PaperProps={{ className: "w-screen max-w-none max-md:max-h-none max-md:h-screen max-md:m-0" }}>
+            <div className="bg-default !bg-opacity-90">
+
+                <div className="pl-2 pr-2 mt-2">
+                    <div className="flex flex-row justify-between h-fit">
+                        <h2 className="text-white m-0">Items da nota fiscal</h2>
+                        <IconButton className="rounded-full outline outline-1 outline-white" color="primary" onClick={closeClick}>
+                            <Close />
+                        </IconButton>
+                    </div>
+
+                    <div className="flex flex-col gap-5 mb-5">
+                        <div className="text-white">Empresa: {props.item.child.Company}</div>
+                        <div className="text-white" style={{ overflowWrap: 'break-word', }}>DANFE: {props.item.child.DanfeCode}</div>
+                        <div className="text-white">Valor total: {clientUtilsUseCases.GetExpensePrice(props.item, { split: false })}</div>
+                    </div>
+                </div>
+
+                <div>
+                    <TableContainer>
+                        <Table>
+                            <TableBody>
+                                <TableRow>
+                                    <TableCell className={defaultCellClass + " bg-default"}>Descrição</TableCell>
+                                    <TableCell className={defaultCellClass + " bg-default"}>Quantidade</TableCell>
+                                    <TableCell className={defaultCellClass + " bg-default"}>UN</TableCell>
+                                    <TableCell className={defaultCellClass + " bg-default"}>Valor Unitário</TableCell>
+                                    <TableCell className={defaultCellClass + " bg-default"}>Valor Total</TableCell>
+                                </TableRow>
+                                {tableRows}
+                            </TableBody>
+                        </Table>
+                    </TableContainer>
+                </div>
+            </div>
+        </Dialog>
+    )
+}
+
+function generateTableRows(item: NfeExpenseChild) {
+    return item.child.nfeitems.map((item) => {
+        return (
+            <TableRow>
+                <TableCell className={defaultCellClass}> {item.Description} </TableCell>
+                <TableCell className={defaultCellClass}> {item.Quantity} </TableCell>
+                <TableCell className={defaultCellClass}> {item.UN} </TableCell>
+                <TableCell className={defaultCellClass}> {item.UnityValue} </TableCell>
+                <TableCell className={defaultCellClass}> {item.TotalValue} </TableCell>
+            </TableRow>
+        )
+    })
+}
+
+interface ComponentsProps {
+    item: NfeExpenseChild
+    open: boolean
+    setOpen: Dispatch<SetStateAction<boolean>>
+}
