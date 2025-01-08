50319ce8f19cad799d0ea9fc34fce5c50e00df86
Tiago Ribeiro <tiagoribeiro12@hotmail.com.br>
2025-01-07 22:04:47 -0300

    corrige tamanho do popUp para poucas linhas add header fixo no mobile corrige tamanho de linhas

--- src/app/pages/components/ExpenseTable/CategoryTableRow/components/NfePopUp.tsx
+++ src/app/pages/components/ExpenseTable/CategoryTableRow/components/NfePopUp.tsx
@@ -14,10 +14,10 @@ export function NfePopUp(props: ComponentsProps) {
     }
 
     return (
-        <Dialog open={props.open} PaperProps={{ className: "w-screen max-w-none max-md:max-h-none max-md:h-screen max-md:m-0" }}>
-            <div className="bg-default !bg-opacity-90">
+        <Dialog open={props.open} PaperProps={{ className: "w-screen max-w-none max-md:max-h-screen max-md:max-h-screen max-md:m-0" }}>
+            <div className="bg-default-light h-full">
 
-                <div className="pl-2 pr-2 mt-2">
+                <div className="pl-2 pr-2 pt-2 pb-2 sticky top-0 bg-default-light">
                     <div className="flex flex-row justify-between h-fit">
                         <h2 className="text-white m-0">Items da nota fiscal</h2>
                         <IconButton className="rounded-full outline outline-1 outline-white" color="primary" onClick={closeClick}>
@@ -54,7 +54,23 @@ export function NfePopUp(props: ComponentsProps) {
 }
 
 function generateTableRows(item: NfeExpenseChild) {
-    return item.child.nfeitems.map((item, index) => {
+
+    let nfeitems = clientUtilsUseCases.handleTableData(item.child.nfeitems)
+
+    return nfeitems.map((item, index) => {
+
+        if (!item) {
+            return (
+                <TableRow key={index}>
+                    <TableCell className="text-slate-800 text-opacity-50 select-none">1</TableCell>
+                    <TableCell className="text-slate-800 text-opacity-50 select-none">1</TableCell>
+                    <TableCell className="text-slate-800 text-opacity-50 select-none">1</TableCell>
+                    <TableCell className="text-slate-800 text-opacity-50 select-none">1</TableCell>
+                    <TableCell className="text-slate-800 text-opacity-50 select-none">1</TableCell>
+                </TableRow>
+            )
+        }
+
         return (
             <TableRow key={index}>
                 <TableCell className={defaultCellClass}> {item.Description} </TableCell>
