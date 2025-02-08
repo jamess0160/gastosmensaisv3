78048bf4024466b7523d361456f1638d7c080f2d
Tiago Ribeiro <tiagoribeiro12@hotmail.com.br>
2025-02-07 23:53:26 -0300

    modificações para suportar funcionalidade de ver senha para inputs com type=passworrd

--- src/app/pages/components/fields/input.tsx
+++ src/app/pages/components/fields/input.tsx
@@ -1,6 +1,11 @@
-import { HTMLAttributes, InputHTMLAttributes } from "react";
+import { Visibility, VisibilityOff } from "@mui/icons-material";
+import { IconButton } from "@mui/material";
+import { Dispatch, HTMLAttributes, InputHTMLAttributes, useState } from "react";
 
 export function Input(props: InputProps) {
+
+    let [isVisible, setIsVisible] = useState(false)
+    let [inputType, setInputType] = useState(props.inputProps?.type)
 
     let label = props.label ? (
         <legend
@@ -11,20 +16,41 @@ export function Input(props: InputProps) {
         </legend>
     ) : null
 
+    let iconVisible = props.inputProps?.type === "password"
+
     return (
         <div className="relative w-full h-8 text-white mix-blend-lighten">
             {label}
             <input
                 {...props.inputProps}
+                type={inputType}
                 className={`
                     w-full box-border pl-4 outline-none
                     bg-default text-xs h-full rounded-xl
                     border border-solid border-white text-white
                     focus:border-gray-500 ${props.inputProps?.className || ""}
                 `}
-            ></input>
+            >
+            </input>
+            <div hidden={iconVisible ? false : true} className="absolute -top-1 right-2">
+                <IconButton color="primary" onClick={() => changeVisibility(setInputType, isVisible, setIsVisible)} >
+                    {isVisible ?
+                        <VisibilityOff />
+                        : <Visibility />
+                    }
+                </IconButton>
+            </div>
         </div>
     )
+}
+
+function changeVisibility(setInputType: Dispatch<string>, currentVisibility: boolean, setIsVisible: Dispatch<boolean>) {
+
+    setInputType(currentVisibility ? "password" : "text")
+
+    console.log("new", !currentVisibility)
+
+    setIsVisible(!currentVisibility)
 }
 
 interface InputProps {
