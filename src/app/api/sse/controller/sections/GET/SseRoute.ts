9bec964cd0cfc202fb20fae44fd7808980500c9b
Tiago Ribeiro <tiagoribeiro12@hotmail.com.br>
2025-02-13 13:46:46 -0300

    reflexo de alteração para clients

--- src/app/api/sse/controller/sections/GET/SseRoute.ts
+++ src/app/api/sse/controller/sections/GET/SseRoute.ts
@@ -1,8 +1,16 @@
 import { serverUtilsUseCases } from "@/useCases/Utils/ServerUtilsUseCases/ServerUtilsUseCases";
+import { NextResponse } from "next/server";
 
 export class SseRoute {
-    public run() {
-        return new Response(serverUtilsUseCases.SseEngine.getReadable(), {
+    public async run() {
+
+        let session = await serverUtilsUseCases.Cookies.getSession()
+
+        if (!session) {
+            return NextResponse.json({ msg: "usuário não logado!" })
+        }
+
+        return new Response(serverUtilsUseCases.SseEngine.addClient(session.IdUser), {
             headers: {
                 'Content-Type': 'text/event-stream',
                 Connection: 'keep-alive',
